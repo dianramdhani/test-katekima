fcc554375ddce2fcb46f3362b3221d37dd901c36
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2025-02-27 16:12:23 +0700

    chore: clean up unnecessary files

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ProductsView from '@/views/ProductsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/products',
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/products',
+      name: 'products',
+      component: ProductsView,
     },
   ],
 })
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-3xl font-bold underline">Products</h1>
+</template>
