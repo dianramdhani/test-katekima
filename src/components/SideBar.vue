84e9885a4ab01857b5a7aec203111af670ddf474
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2025-02-28 21:05:41 +0700

    fix: minor typo

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -6,7 +6,7 @@ import { RouterLink } from 'vue-router'
   <aside class="drawer-side">
     <label for="sidebar-drawer" class="drawer-overlay"></label>
     <div class="menu p-4 w-64 bg-base-200 min-h-full">
-      <h2 class="text-xl font-bold mb-4">Kitekima</h2>
+      <h2 class="text-xl font-bold mb-4">Katekima</h2>
       <ul>
         <li>
           <RouterLink to="/products">{{ $t('product.product') }}</RouterLink>
